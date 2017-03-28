6c773773e284fb47471e89982cb432b32c04eecf
Arete Code <aretecode@gmail.com>
2017-03-28 00:04:14 -0700

    ℹ️️ add example comments for merging in Config 👕⚒ fixed linting according to review 🎁 added string merging for output

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const ChainedMap = require('./ChainedMap');
 const ChainedSet = require('./ChainedSet');
 const Resolve = require('./Resolve');
@@ -7,7 +8,6 @@ const DevServer = require('./DevServer');
 const Plugin = require('./Plugin');
 const Module = require('./Module');
 const Performance = require('./Performance');
-const isClass = require('izz-class')
 
 module.exports = class extends ChainedMap {
   constructor() {
@@ -81,7 +81,6 @@ module.exports = class extends ChainedMap {
 
         switch (key) {
           case 'node':
-          case 'output':
           case 'resolve':
           case 'resolveLoader':
           case 'devServer':
@@ -89,12 +88,55 @@ module.exports = class extends ChainedMap {
             return this[key].merge(value);
           }
 
+          /**
+           * @example:
+           *
+           * input:
+           * '/code/example/dist/[name].js'
+           *
+           * output:
+           * {
+           *   path: '/code/example/dist',
+           *   filename: '[name].js',
+           * }
+           *
+           *
+           */
+          case 'output': {
+            if (typeof value === 'string') {
+              const filename = path.basename(value);
+              const dir = path.dirname(value);
+              const asObject = {
+                path: dir,
+                filename,
+              };
+
+              return this.output.merge(asObject);
+            }
+          }
+
+          /**
+           * take a string entry,
+           * file name becomes property,
+           * value becomes an array,
+           * if the same prop exists,
+           * it is merged in with existing values
+           *
+           * @example:
+           *
+           * input: './src/front/index.js'
+           * output: {index: ['./src/front/index.js']}
+           */
           case 'entry': {
             if (typeof value === 'string') {
-              const path = require('path');
-              const name = path.basename(value);
+              let name = path.basename(value);
+              if (name.includes('.')) {
+                name = name.split('.').shift();
+              }
+
               return this.entry(name).merge([value]);
             }
+
             return Object
               .keys(value)
               .forEach(name => this.entry(name).merge(value[name]));
@@ -106,13 +148,25 @@ module.exports = class extends ChainedMap {
               .forEach(name => this.plugin(name).merge(value[name]));
           }
 
+          /**
+           * @see ./Plugin
+           * merge an array of plugins
+           */
           case 'plugins': {
             if (Array.isArray(value)) {
               return value
                 .forEach((plugin, index) => {
-                  if (plugin.name) index = plugin.name;
-                  if (isClass(plugin)) this.plugin(index).plugin(plugin);
-                  else this.plugin(index).init((args) => plugin);
+                  plugin.name = plugin.name || index
+
+                  // if Class, default Plugin.init will instantiate it
+                  if (toString.call(plugin) === '[object Function]') {
+                    this.plugin(plugin.name).plugin(plugin);
+                  }
+
+                  // otherwise, it is already instantiated
+                  else {
+                    this.plugin(plugin.name).init((args) => plugin);
+                  }
                 });
             }
           }
