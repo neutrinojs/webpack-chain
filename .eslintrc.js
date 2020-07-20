74b1791e8513b3f837fbb63d687d223991ecb661
Ed Morley <501702+edmorley@users.noreply.github.com>
2020-07-20 16:38:01 +0100

    Clean up linting configuration (#268)
    * Switch back from `babel-eslint` to the default ESLint parser, since
      it supports all of the syntax we are using out of the box.
    * Use the new `ignorePatterns` option instead of `.eslintignore`.
    * Adjust the ignore pattern from `!.eslintrc.js` to `!.*.js` so that
      all dotfiles are ignored by default, which also allows us to remove
      the multiple globbing combinations in the ESLint CLI command used for
      `yarn lint`, and instead pass just `.`.
    * Use the new `reportUnusedDisableDirectives` option instead of the
      `--report-unused-disable-directives` CLI flag.
    * Enable `--max-warnings 0` so that CI fails for warnings too.
    * Move the Prettier config out of `.eslintrc.js` to `.prettierrc.js`,
      since not all editor integrations check the ESLint config location.
    * Switch from running Prettier as an ESLint plugin to using the native
      CLI, since it's faster, the UX is better when using editor
      integrations (eg prevents lots of red underlines which will be fixed
      on save anyway), and it means non-JS file types can be formatted.
      Additional `package.json` scripts have been added to make it easier
      for those who don't use format-on-save to apply fixes.
    * The previously unchecked files (`*.json` and files under `types/`)
      have had their formatting fixed using `yarn fix`.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,17 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const {
-  rules: airbnbBaseStyle,
-} = require('eslint-config-airbnb-base/rules/style');
-const {
-  rules: airbnbBaseBestPractices,
-} = require('eslint-config-airbnb-base/rules/best-practices');
-
 module.exports = {
+  root: true,
   extends: [
     'eslint-config-airbnb-base',
-    'plugin:prettier/recommended',
-    'prettier/babel',
+    'eslint-config-prettier',
     'plugin:jest/recommended',
     'plugin:jest/style',
   ],
-  plugins: ['eslint-plugin-babel'],
-  parser: 'babel-eslint',
-  env: {
-    es6: true,
-    commonjs: true,
-    node: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
+  // Force dotfiles to be checked, since by default ESLint ignores them.
+  ignorePatterns: ['!.*.js'],
+  reportUnusedDisableDirectives: true,
   rules: {
-    // Disable rules for which there are eslint-plugin-babel replacements:
-    // https://github.com/babel/eslint-plugin-babel#rules
-    'new-cap': 'off',
-    'no-invalid-this': 'off',
-    'object-curly-spacing': 'off',
-    semi: 'off',
-    'no-unused-expressions': 'off',
-    // Ensure the replacement rules use the options set by airbnb rather than ESLint defaults.
-    'babel/new-cap': airbnbBaseStyle['new-cap'],
-    'babel/no-invalid-this': airbnbBaseBestPractices['no-invalid-this'],
-    'babel/object-curly-spacing': airbnbBaseStyle['object-curly-spacing'],
-    'babel/semi': airbnbBaseStyle.semi,
-    'babel/no-unused-expressions':
-      airbnbBaseBestPractices['no-unused-expressions'],
     'class-methods-use-this': 'off',
     'no-shadow': 'off',
     'no-underscore-dangle': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        jsxBracketSameLine: true,
-        trailingComma: 'all',
-        proseWrap: 'always',
-        endOfLine: 'lf',
-      },
-    ],
   },
 };
--- types/test/webpack-chain-tests.ts
+++ types/test/webpack-chain-tests.ts
@@ -28,10 +28,10 @@ config
   .externals(['foo', 'bar'])
   .externals((context, request, cb) => cb(null, 'foo'))
   .loader({})
-  .name("config-name")
-  .mode("none")
-  .mode("development")
-  .mode("production")
+  .name('config-name')
+  .mode('none')
+  .mode('development')
+  .mode('production')
   .profile(false)
   .parallelism(2)
   .recordsPath('')
@@ -40,321 +40,304 @@ config
   .stats({
     assets: false,
     publicPath: true,
-    modules: false
+    modules: false,
   })
   .target('web')
   .watch(true)
   .watchOptions({})
-  .when(false, config => config.watch(true), config => config.watch(false))
+  .when(
+    false,
+    (config) => config.watch(true),
+    (config) => config.watch(false),
+  )
 
   .entry('main')
-    .add('index.js')
-    .delete('index.js')
-    .clear()
-    .when(false, entry => entry.clear(), entry => entry.clear())
-    .end()
+  .add('index.js')
+  .delete('index.js')
+  .clear()
+  .when(
+    false,
+    (entry) => entry.clear(),
+    (entry) => entry.clear(),
+  )
+  .end()
 
-  .entryPoints
-    .delete('main')
-    .end()
+  .entryPoints.delete('main')
+  .end()
 
-  .output
-    .futureEmitAssets(true)
-    .auxiliaryComment('Test Comment')
-    .auxiliaryComment({
-      root: 'Root Comment'
-    })
-    .chunkFilename('')
-    .chunkLoadTimeout(1000)
-    .crossOriginLoading(true)
-    .devtoolFallbackModuleFilenameTemplate('')
-    .devtoolNamespace('')
-    .devtoolLineToLine('')
-    .devtoolModuleFilenameTemplate('')
-    .filename('main.js')
-    .globalObject('global')
-    .hashFunction('md5')
-    .hashDigest('md5')
-    .hashDigestLength(15)
-    .hashSalt('')
-    .hotUpdateChunkFilename('update')
-    .hotUpdateFunction(() => {})
-    .hotUpdateMainFilename('main')
-    .jsonpFunction('callback')
-    .library('var')
-    .libraryExport(['MyModule', 'MySubModule'])
-    .libraryTarget('var')
-    .path('/')
-    .pathinfo(true)
-    .publicPath('/')
-    .sourceMapFilename('index.js.map')
-    .sourcePrefix('~')
-    .strictModuleExceptionHandling(true)
-    .umdNamedDefine(true)
-    .end()
+  .output.futureEmitAssets(true)
+  .auxiliaryComment('Test Comment')
+  .auxiliaryComment({
+    root: 'Root Comment',
+  })
+  .chunkFilename('')
+  .chunkLoadTimeout(1000)
+  .crossOriginLoading(true)
+  .devtoolFallbackModuleFilenameTemplate('')
+  .devtoolNamespace('')
+  .devtoolLineToLine('')
+  .devtoolModuleFilenameTemplate('')
+  .filename('main.js')
+  .globalObject('global')
+  .hashFunction('md5')
+  .hashDigest('md5')
+  .hashDigestLength(15)
+  .hashSalt('')
+  .hotUpdateChunkFilename('update')
+  .hotUpdateFunction(() => {})
+  .hotUpdateMainFilename('main')
+  .jsonpFunction('callback')
+  .library('var')
+  .libraryExport(['MyModule', 'MySubModule'])
+  .libraryTarget('var')
+  .path('/')
+  .pathinfo(true)
+  .publicPath('/')
+  .sourceMapFilename('index.js.map')
+  .sourcePrefix('~')
+  .strictModuleExceptionHandling(true)
+  .umdNamedDefine(true)
+  .end()
 
-  .resolve
-    .cachePredicate(({ path, request }) => true)
-    .cacheWithContext(true)
-    .enforceExtension(true)
-    .enforceModuleExtension(true)
-    .unsafeCache(false)
-    .unsafeCache(/foo/)
-    .symlinks(true)
-    .alias
-      .set('foo', 'bar')
-      .end()
-    .modules
-      .add('index.js')
-      .end()
-    .aliasFields
-      .add('foo')
-      .end()
-    .descriptionFiles
-      .add('foo')
-      .end()
-    .extensions
-      .add('.js')
-      .end()
-    .mainFields
-      .add('browser')
-      .end()
-    .mainFiles
-      .add('index.js')
-      .end()
-    .plugin('foo')
-      .use(ResolvePluginImpl, [])
-      .end()
-    .plugins
-      .delete('foo')
-      .end()
-    .end()
+  .resolve.cachePredicate(({ path, request }) => true)
+  .cacheWithContext(true)
+  .enforceExtension(true)
+  .enforceModuleExtension(true)
+  .unsafeCache(false)
+  .unsafeCache(/foo/)
+  .symlinks(true)
+  .alias.set('foo', 'bar')
+  .end()
+  .modules.add('index.js')
+  .end()
+  .aliasFields.add('foo')
+  .end()
+  .descriptionFiles.add('foo')
+  .end()
+  .extensions.add('.js')
+  .end()
+  .mainFields.add('browser')
+  .end()
+  .mainFiles.add('index.js')
+  .end()
+  .plugin('foo')
+  .use(ResolvePluginImpl, [])
+  .end()
+  .plugins.delete('foo')
+  .end()
+  .end()
 
-  .resolveLoader
-    .moduleExtensions
-      .add('.js')
-      .end()
-    .packageMains
-      .add('index.js')
-      .end()
-    .plugin('foo')
-      .use(webpack.DefinePlugin)
-      .end()
-    .end()
+  .resolveLoader.moduleExtensions.add('.js')
+  .end()
+  .packageMains.add('index.js')
+  .end()
+  .plugin('foo')
+  .use(webpack.DefinePlugin)
+  .end()
+  .end()
 
-  .performance
-    .hints(true)
-    .hints('warning')
-    .maxEntrypointSize(20000)
-    .maxAssetSize(20000)
-    .assetFilter(filename => true)
-    .end()
+  .performance.hints(true)
+  .hints('warning')
+  .maxEntrypointSize(20000)
+  .maxAssetSize(20000)
+  .assetFilter((filename) => true)
+  .end()
 
-  .optimization
-    .concatenateModules(true)
-    .flagIncludedChunks(true)
-    .mergeDuplicateChunks(true)
-    .minimize(true)
-    .namedChunks(true)
-    .namedModules(true)
-    .nodeEnv(true)
-    .noEmitOnErrors(true)
-    .occurrenceOrder(true)
-    .portableRecords(true)
-    .providedExports(true)
-    .removeAvailableModules(true)
-    .removeEmptyChunks(true)
-    .runtimeChunk("single")
-    .runtimeChunk({ name: ({}) => "hello" })
-    .sideEffects(true)
-    .splitChunks({})
-    .usedExports(true)
-    .minimizer('foo')
-      .use(webpack.DefinePlugin)
-      .tap((config) => [])
-      .end()
-    .end()
+  .optimization.concatenateModules(true)
+  .flagIncludedChunks(true)
+  .mergeDuplicateChunks(true)
+  .minimize(true)
+  .namedChunks(true)
+  .namedModules(true)
+  .nodeEnv(true)
+  .noEmitOnErrors(true)
+  .occurrenceOrder(true)
+  .portableRecords(true)
+  .providedExports(true)
+  .removeAvailableModules(true)
+  .removeEmptyChunks(true)
+  .runtimeChunk('single')
+  .runtimeChunk({ name: ({}) => 'hello' })
+  .sideEffects(true)
+  .splitChunks({})
+  .usedExports(true)
+  .minimizer('foo')
+  .use(webpack.DefinePlugin)
+  .tap((config) => [])
+  .end()
+  .end()
 
   .plugin('foo')
-    .use(webpack.DefinePlugin, [{
-      'process.env.NODE_ENV': ""
-    }])
-    .end()
+  .use(webpack.DefinePlugin, [
+    {
+      'process.env.NODE_ENV': '',
+    },
+  ])
+  .end()
 
   .plugin('bar')
-    .use(webpack.DefinePlugin, [{
-      'process.env.NODE_ENV': ""
-    }])
-    .before('foo')
-    .end()
+  .use(webpack.DefinePlugin, [
+    {
+      'process.env.NODE_ENV': '',
+    },
+  ])
+  .before('foo')
+  .end()
 
   .plugin('baz')
-    .use(webpack.DefinePlugin, [{
-      'process.env.NODE_ENV': ""
-    }])
-    .after('bar')
-    .end()
+  .use(webpack.DefinePlugin, [
+    {
+      'process.env.NODE_ENV': '',
+    },
+  ])
+  .after('bar')
+  .end()
 
   .plugin('asString')
-    .use('package-name-or-path')
-    .end()
+  .use('package-name-or-path')
+  .end()
 
   .plugin('asObject')
-    .use({ apply: (compiler: webpack.Compiler) => {}})
-    .end()
+  .use({ apply: (compiler: webpack.Compiler) => {} })
+  .end()
 
-  .plugins
-    .delete('foo')
-    .delete('bar')
-    .delete('baz')
-    .delete('asString')
-    .delete('asObject')
-    .end()
+  .plugins.delete('foo')
+  .delete('bar')
+  .delete('baz')
+  .delete('asString')
+  .delete('asObject')
+  .end()
 
-  .node
-    .set('__dirname', true)
-    .delete('__dirname')
-    .clear()
-    .end()
+  .node.set('__dirname', true)
+  .delete('__dirname')
+  .clear()
+  .end()
 
-  .devServer
-    .allowedHosts
-      .add('host.com')
-      .clear()
-      .end()
-    .after(() => {})
-    .before(() => {})
-    .bonjour(true)
-    .clientLogLevel('error')
-    .color(true)
-    .compress(false)
-    .contentBase('/')
-    .contentBase(['foo', 'bar'])
-    .disableHostCheck(true)
-    .filename('hello')
-    .headers({
-      'Content-Type': 'text/css',
-    })
-    .historyApiFallback(true)
-    .host('localhost')
-    .hot(true)
-    .hotOnly(true)
-    .http2(true)
-    .https(true)
-    .index('test.html')
-    .info(true)
-    .inline(true)
-    .lazy(true)
-    .mimeTypes({'text/html': ['phtml']})
-    .noInfo(true)
-    .open(true)
-    .openPage('/foo')
-    .openPage(['/foo', '/bar'])
-    .overlay(true)
-    .overlay({
-      warnings: true,
-      errors: true,
-    })
-    .pfx('/path/to/file.pfx')
-    .pfxPassphrase('passphrase')
-    .port(8080)
-    .progress(true)
-    .proxy({})
-    .public('foo')
+  .devServer.allowedHosts.add('host.com')
+  .clear()
+  .end()
+  .after(() => {})
+  .before(() => {})
+  .bonjour(true)
+  .clientLogLevel('error')
+  .color(true)
+  .compress(false)
+  .contentBase('/')
+  .contentBase(['foo', 'bar'])
+  .disableHostCheck(true)
+  .filename('hello')
+  .headers({
+    'Content-Type': 'text/css',
+  })
+  .historyApiFallback(true)
+  .host('localhost')
+  .hot(true)
+  .hotOnly(true)
+  .http2(true)
+  .https(true)
+  .index('test.html')
+  .info(true)
+  .inline(true)
+  .lazy(true)
+  .mimeTypes({ 'text/html': ['phtml'] })
+  .noInfo(true)
+  .open(true)
+  .openPage('/foo')
+  .openPage(['/foo', '/bar'])
+  .overlay(true)
+  .overlay({
+    warnings: true,
+    errors: true,
+  })
+  .pfx('/path/to/file.pfx')
+  .pfxPassphrase('passphrase')
+  .port(8080)
+  .progress(true)
+  .proxy({})
+  .public('foo')
 
-    .publicPath('bar')
-    .quiet(false)
-    .setup(app => {})
-    .socket('socket')
-    .sockHost('localhost')
-    .sockPath('/sockpath/')
-    .sockPort(8080)
-    .staticOptions({})
-    .stats({
-      reasons: true,
-      errors: true,
-      warnings: false,
-    })
-    .stdin(true)
-    .useLocalIp(true)
-    .watchContentBase(true)
-    .watchOptions({})
-    .writeToDisk(true)
-    .end()
+  .publicPath('bar')
+  .quiet(false)
+  .setup((app) => {})
+  .socket('socket')
+  .sockHost('localhost')
+  .sockPath('/sockpath/')
+  .sockPort(8080)
+  .staticOptions({})
+  .stats({
+    reasons: true,
+    errors: true,
+    warnings: false,
+  })
+  .stdin(true)
+  .useLocalIp(true)
+  .watchContentBase(true)
+  .watchOptions({})
+  .writeToDisk(true)
+  .end()
 
-  .module
-    .noParse(/.min.js$/)
-    .strictExportPresence(true)
-    .rule('compile')
-      .test(/.js$/)
-      .include
-        .add(/.js$/)
-        .end()
-      .exclude
-        .add(/node_modules/)
-        .end()
-      .parser({
-        opt: 'foo',
-      })
-      .enforce('pre')
-      .use('babel')
-        .tap((config) => [])
-        .loader('babel-loader')
-        .options({})
-        .end()
-      .use('eslint')
-        .loader('eslint-loader')
-        .options({})
-        .after('babel')
-        .end()
-      .uses
-        .delete('babel')
-        .delete('eslint')
-        .end()
-      .pre()
-      .post()
-      .rule('inline')
-        .after('vue')
-        .resourceQuery(/inline/)
-        .use('url')
-          .loader('url-loader')
-          .end()
-        .resolve
-          .symlinks(true)
-          .end()
-        .end()
-      .rules
-        .delete('inline')
-        .end()
-      .oneOf('inline')
-        .after('vue')
-        .uses
-          .delete('babel')
-          .end()
-        .resourceQuery(/inline/)
-        .use('url')
-          .loader('url-loader')
-          .end()
-        .end()
-      .oneOfs
-        .delete('inline')
-        .end()
-      .resolve
-        .symlinks(true)
-        .end()
-      .end()
-    .rules
-      .delete('compile')
-      .end()
-    .end()
+  .module.noParse(/.min.js$/)
+  .strictExportPresence(true)
+  .rule('compile')
+  .test(/.js$/)
+  .include.add(/.js$/)
+  .end()
+  .exclude.add(/node_modules/)
+  .end()
+  .parser({
+    opt: 'foo',
+  })
+  .enforce('pre')
+  .use('babel')
+  .tap((config) => [])
+  .loader('babel-loader')
+  .options({})
+  .end()
+  .use('eslint')
+  .loader('eslint-loader')
+  .options({})
+  .after('babel')
+  .end()
+  .uses.delete('babel')
+  .delete('eslint')
+  .end()
+  .pre()
+  .post()
+  .rule('inline')
+  .after('vue')
+  .resourceQuery(/inline/)
+  .use('url')
+  .loader('url-loader')
+  .end()
+  .resolve.symlinks(true)
+  .end()
+  .end()
+  .rules.delete('inline')
+  .end()
+  .oneOf('inline')
+  .after('vue')
+  .uses.delete('babel')
+  .end()
+  .resourceQuery(/inline/)
+  .use('url')
+  .loader('url-loader')
+  .end()
+  .end()
+  .oneOfs.delete('inline')
+  .end()
+  .resolve.symlinks(true)
+  .end()
+  .end()
+  .rules.delete('compile')
+  .end()
+  .end()
 
   //** support https://webpack.js.org/configuration/module/#ruletype  */
-  .module
-    .rule('mjs-compile')
-      .test(/\.mjs$/)
-      .type('javascript/auto')
-      .end()
-    .end()
+  .module.rule('mjs-compile')
+  .test(/\.mjs$/)
+  .type('javascript/auto')
+  .end()
+  .end()
 
   .merge({})
   .toConfig();
@@ -366,17 +349,27 @@ expectType<typeof entryPoints>(entryPoints.clear());
 expectType<typeof entryPoints>(entryPoints.delete('key'));
 expectType<boolean>(entryPoints.has('key'));
 expectType<Config.EntryPoint>(entryPoints.get('key'));
-expectType<Config.EntryPoint>(entryPoints.getOrCompute('key', () => new Config.EntryPoint()));
+expectType<Config.EntryPoint>(
+  entryPoints.getOrCompute('key', () => new Config.EntryPoint()),
+);
 expectType<typeof entryPoints>(entryPoints.set('key', new Config.EntryPoint()));
-expectType<typeof entryPoints>(entryPoints.merge({
-  key: new Config.EntryPoint(),
-}));
+expectType<typeof entryPoints>(
+  entryPoints.merge({
+    key: new Config.EntryPoint(),
+  }),
+);
 expectType<Record<string, Config.EntryPoint>>(entryPoints.entries());
-expectType<typeof entryPoints>(entryPoints.when(true, (val) => {
-  expectType<typeof entryPoints>(val);
-}, (val) => {
-  expectType<typeof entryPoints>(val);
-}));
+expectType<typeof entryPoints>(
+  entryPoints.when(
+    true,
+    (val) => {
+      expectType<typeof entryPoints>(val);
+    },
+    (val) => {
+      expectType<typeof entryPoints>(val);
+    },
+  ),
+);
 
 // Test TypedChainedSet
 const extensions = config.resolve.extensions;
@@ -388,8 +381,14 @@ expectType<typeof extensions>(extensions.delete('.txt'));
 expectType<boolean>(extensions.has('.txt'));
 expectType<typeof extensions>(extensions.merge(['.txt']));
 expectType<string[]>(extensions.values());
-expectType<typeof extensions>(extensions.when(true, (val) => {
-  expectType<typeof extensions>(val);
-}, (val) => {
-  expectType<typeof extensions>(val);
-}));
+expectType<typeof extensions>(
+  extensions.when(
+    true,
+    (val) => {
+      expectType<typeof extensions>(val);
+    },
+    (val) => {
+      expectType<typeof extensions>(val);
+    },
+  ),
+);
